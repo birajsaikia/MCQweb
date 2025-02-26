1678a89b3e7bd5b6617180c2e91efb30d3d5bcd1
unknown <birajjyo2@gmail.com>
2025-02-26 11:51:08 +0530

    first commit

--- src/Component/Hearder.jsx
+++ src/Component/Hearder.jsx
@@ -73,7 +73,6 @@ const Header = () => {
         </Link>
       </div>
 
-      {/* Hamburger Menu for Small Screens */}
       <div
         className="menu-icon"
         onClick={() => setMenuOpen(!menuOpen)}
